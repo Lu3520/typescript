bbfdd6e66d4c2f66fef2ae8607c4573d30601507
Lu3520 <leo24041127@gmail.com>
2023-10-18 11:44:02 +0900

    静的なリストを追加とv-forの使い方

--- vue-sample/src/components/Tweet.vue
+++ vue-sample/src/components/Tweet.vue
@@ -1,8 +1,24 @@
 <script setup lang="ts">
+const tweets = [{ id: '0', description: 'Hello World'}, { id: '1', description: 'this is the second'}]
 </script>
 
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <h1>Tweeter</h1>
+    <div class="form-container">
+        <input />
+        <button class="save-button">post</button>
+    </div>
+    <div class="tweet-container">
+        <ul>
+            <!-- keyを使う理由 -->
+            <!-- https://reffect.co.jp/vue/v-bind-key-understand-by-developer-tool/#v-bindkey-%E3%82%92%E4%BD%BF%E7%94%A8%E3%81%99%E3%82%8B%E5%A0%B4%E5%90%88 -->
+            <li v-for="tweet in tweets" :key="tweet.id" class="tweet-list">
+                <span>{{ tweet.description }}</span>
+            </li>
+        </ul>
+    </div>
+  </div>
 </template>
 
 <style scoped>
@@ -18,8 +34,27 @@
   align-items: center;
   background-color: aliceblue;
   padding: 24px 0;
-  width: 60%;
+  width: 500px;
   margin-bottom: 12px;
   border-radius: 4px;
+}
+
+.tweet-list{
+    position: relative;
+    right: 10px;
+    display: flex;
+    list-style: none;
+    align-items: center;
+}
+
+button {
+    color: white;
+    font-weight: bold;
+    background-color: #68c9c9;
+    margin-top: 8px;
+    border-radius: 2px;
+    border: none;
+    width: auto;
+    height: auto;
 }
 </style>
