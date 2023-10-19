bde74fbb39c899c823ad3d85f67b31616409825e
Lu3520 <leo24041127@gmail.com>
2023-10-19 15:15:59 +0900

    動的なstyleの書き方 v-bind:disabledの使用方法

--- vue-sample/src/components/PersonPostForm.vue
+++ vue-sample/src/components/PersonPostForm.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, defineEmits } from 'vue'
+import { ref, defineEmits, computed } from 'vue'
 const inputName = ref<string>('')
 const inputAge = ref<number>(0)
 
@@ -11,6 +11,19 @@ const register = () => {
     emit('register', person)
 }
 
+const nameLengthLimit = 5
+const isValidName = computed(() => {
+    if (inputName.value.length >= nameLengthLimit) {
+       return false
+    } else {
+        return true
+    }
+})
+
+const color = computed(() => {
+    return isValidName.value ? 'white' : 'rgb(244, 194, 190)'
+})
+
 </script>
 
 <template>
@@ -18,14 +31,17 @@ const register = () => {
         <div class="input-container">
             <div class="input-column">
                 <span>name:</span>
-                <input class="input" v-model="inputName"/>
+                <input class="input-name" v-model="inputName"/>
             </div>
+            <span class="error-text" v-show="!isValidName">{{ nameLengthLimit }} characters or less</span>
             <div class="input-column">
                 <span>age:</span>
                 <input class="input" v-model="inputAge" type="number"/>
             </div>
         </div>
-        <button @click="register" class="register-button">register</button>
+        <div>
+            <button :disabled="!isValidName" @click="register" class="register-button">register</button>
+        </div>
     </div>
 </template>
 
@@ -46,6 +62,12 @@ span {
     font-weight: bold;
 }
 
+.error-text {
+    font-size: 10px;
+    position: relative;
+    bottom: 3px;
+}
+
 .input-column {
     widows: 200px;
     display: flex;
@@ -57,12 +79,22 @@ input {
     width: 150px;
 }
 
+.input-name {
+    /* scriptの変数を使用する方法 */
+    background-color: v-bind(color);
+}
+
 .input-container {
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     height: 50px;
     margin-bottom: 20px;
+}
+
+.register-button {
+    position: relative;
+    top: 15px;
 }
 
 button {
